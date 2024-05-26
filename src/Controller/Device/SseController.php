75c734ee346c45b0e415810d7b2f5eb4b8bed6e2
Renato Sousa <renatosousa@sousadev.me>
2024-05-26 19:04:42 -0300

    removendo interface do symfony para sse

--- src/Controller/Device/SseController.php
+++ src/Controller/Device/SseController.php
@@ -12,30 +12,20 @@ class SseController extends AbstractController
     #[Route('/test', name: 'device_monitoring', methods: ['GET'])]
     public function index(
         LoggerInterface $logger
-    ): StreamedResponse
+    )
     {
-        $response = new StreamedResponse();
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        header('X-Accel-Buffering: no');
 
         $logger->info('SSE connection opened');
 
-        // Set headers for SSE
-        $response->headers->set('Content-Type', 'text/event-stream');
-        $response->headers->set('Cache-Control', 'no-cache');
-        $response->headers->set('Connection', 'keep-alive');
-        $response->headers->set('X-Accel-Buffering', 'no');
-
-
-        // Example data stream
-        $response->setCallback(function () {
-            while(true){
-                echo "data: " . json_encode(['time' => time()]) . "\n\n";
-                ob_flush();
-                flush();
-                sleep(1);
-            
-            }
-        });
-
-        return $response;
+        while(true){
+            echo "data: " . json_encode(['time' => time()]) . "\n\n";
+            ob_flush();
+            flush();
+            sleep(1);
+        }
+    
     }
 }
