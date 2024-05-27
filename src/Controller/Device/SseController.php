713730199100017c0d653c8bc329a46af7d07ba6
Renato Sousa <renatosousa@sousadev.me>
2024-05-26 22:24:11 -0300

    .

--- src/Controller/Device/SseController.php
+++ src/Controller/Device/SseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Device;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,37 +10,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SseController extends AbstractController
 {
     #[Route('/test', name: 'device_monitoring', methods: ['GET'])]
-    public function sseAction(): StreamedResponse
+    public function index(
+        LoggerInterface $logger
+    )
     {
-        // Set response headers
-        $response = new StreamedResponse();
-        $response->headers->set('Content-Type', 'text/event-stream');
-        $response->headers->set('Cache-Control', 'no-cache');
-        $response->headers->set('X-Accel-Buffering', 'no');
-        $response->headers->set('Connection', 'keep-alive');
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        header('X-Accel-Buffering: no');
+        header('Connection: keep-alive');
+        set_time_limit(0);
 
-        // Send data periodically
-        $response->setCallback(function () {
-            while (true) {
+        $logger->info('SSE connection opened');
 
-                if(connection_aborted()) {
-                    exit();
-                }
+        echo "data: " . json_encode(['time' => time()]) . "\n\n";
+        ob_flush();
+        flush();
 
-                // Fetch data to send
-                $data = (new \DateTime('now'))->format('Y-m-d H:i:s');
-
-                // Send data to client
-                echo "data: " . json_encode($data) . "\n\n";
-                flush();
-
-                // Send a heartbeat message every 25 seconds
-                sleep(5);
-                echo ":\n\n"; // Heartbeat message
-                flush();
-            }
-        });
-
-        return $response;
+        while(true){
+            echo "data: " . json_encode(['time' => time()]) . "\n\n";
+            ob_flush();
+            flush();
+            sleep(1);
+        }
+    
     }
 }
