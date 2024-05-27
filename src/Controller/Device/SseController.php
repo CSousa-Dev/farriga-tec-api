ee99a1611383ba24ee9af0b9861a55c6429e886c
Renato Sousa <renatosousa@sousadev.me>
2024-05-26 22:21:48 -0300

    .

--- src/Controller/Device/SseController.php
+++ src/Controller/Device/SseController.php
@@ -9,17 +9,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SseController extends AbstractController
 {
     #[Route('/test', name: 'device_monitoring', methods: ['GET'])]
-    public function sseAction()
+    public function sseAction(): StreamedResponse
     {
         // Set response headers
         $response = new StreamedResponse();
         $response->headers->set('Content-Type', 'text/event-stream');
         $response->headers->set('Cache-Control', 'no-cache');
         $response->headers->set('X-Accel-Buffering', 'no');
+        $response->headers->set('Connection', 'keep-alive');
 
         // Send data periodically
         $response->setCallback(function () {
             while (true) {
+
+                if(connection_aborted()) {
+                    exit();
+                }
+
                 // Fetch data to send
                 $data = (new \DateTime('now'))->format('Y-m-d H:i:s');
 
@@ -28,7 +34,7 @@ class SseController extends AbstractController
                 flush();
 
                 // Send a heartbeat message every 25 seconds
-                sleep(25);
+                sleep(5);
                 echo ":\n\n"; // Heartbeat message
                 flush();
             }
