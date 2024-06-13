93b95aebe32bd43f972ad773702ee4b932ebb9d9
Renato Sousa <renatosousa@sousadev.me>
2024-06-13 17:44:26 -0300

    removendo namespace nao utilizado

--- src/Controller/Account/AuthController.php
+++ src/Controller/Account/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Account;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Application\NotConfirmedEmailException;
 use App\Infra\Doctrine\Entity\Security\ApiToken;
 use App\Infra\Doctrine\Entity\Security\UserSecurityInfo;
 use App\Infra\Doctrine\Repository\Security\ApiTokenRepository;
